cfed7ff4eb421b8b2b16d7f386662fe81f217cd9
egi papa <egipapa@gmail.com>
2020-08-11 15:44:13 +0200

    11_fibonacci

---
+++ NewView/NewView/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NewView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPagee());
+        }
+    }
+}
---
+++ NewView/NewView/SecondPagee.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NewView
+{
+    public partial class SecondPagee : ContentPage
+    {
+        public SecondPagee()
+        {
+            InitializeComponent();
+        }
+        public long fib(long Value)
+        {
+            if (Value == 0)
+            {
+                return 0;
+            }
+            else if (Value == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fib(Value - 1) + fib(Value - 2);
+            }
+        }
+    }
+}
